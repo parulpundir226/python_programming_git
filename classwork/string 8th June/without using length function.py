8fd3c473a0a7e6eae8e0637452c737113c76a4cb
parul pundir <pundirparul2612@gmail.com>
2026-06-08 15:29:15 +0530

    character counting without using length function.py

---
+++ classwork/string 8th June/without using length function.py
@@ -0,0 +1,12 @@
+# Input a string or sentence from the user
+text = input("Enter a string or sentence: ")
+
+# Variable to count characters
+count = 0
+
+# Loop through each character
+for ch in text:
+    count = count + 1
+
+# Display total number of characters
+print("Total number of characters:", count)
